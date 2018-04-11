0c704045e4adc76244a979a27cff7bc1cc953f05
shannonanderson77 <shannonanderson77@gmail.com>
2018-04-11 22:15:09 +0000

    00...99

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,7 +1,4 @@
 #!/usr/bin/python3
-for i in range(10):
-    for j in range(10):
-        if j != 9 or i != 9:
-            print("{}{}, ".format(i, j), end="")
-        else:
-            print("{}{}".format(i, j))
+for i in range(99):
+    print("{:02d}, ".format(i), end="")
+print("{}".format(i + 1))
