6953f32148043b071f79716e9be68e93baa610ac
shannonanderson77 <shannonanderson77@gmail.com>
2018-04-18 04:10:03 +0000

    change max variable name to maximum

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 def max_integer(my_list=[]):
-    max = 0
+    maximum = 0
     if my_list is None:
         return (None)
     for i in range(len(my_list) - 1):
-        if my_list[i] > my_list[i + 1]:
-            max = my_list[i]
+        if my_list[i + 1] > my_list[i]:
+            maximum = my_list[i + 1]
         else:
-            max = my_list[i + 1]
-    return (max)
+            maximum = my_list[i]
+    return (maximum)
