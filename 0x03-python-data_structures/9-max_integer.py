a4c35d73f0f6fe21321fbdcb59e5826c5cd032df
shannonanderson77 <shannonanderson77@gmail.com>
2018-04-18 22:50:10 +0000

    change code to use sort, fix some obscure edge case

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -2,5 +2,5 @@
 def max_integer(my_list=[]):
     if my_list is None:
         return (None)
-    maximum = sorted(my_list)[-1:]
-    return (maximum[0])
+    my_list.sort()
+    return (my_list[-1])
