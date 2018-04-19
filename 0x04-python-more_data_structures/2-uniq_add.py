7a8bd4cdfbfa70143b19e779d47acdcd93743e34
shannonanderson77 <shannonanderson77@gmail.com>
2018-04-19 19:33:53 +0000

    add check for empty list

--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -2,6 +2,8 @@
 def uniq_add(my_list=[]):
     total = 0
     unique_list = list(set(my_list))
+    if not my_list:
+        return (0)
     for i in range(len(unique_list)):
         total += unique_list[i]
     return (total)
