9af33439b790e7c6d0394b41e7817e7d87f3f6c8
shannonanderson77 <shannonanderson77@gmail.com>
2018-04-20 00:07:01 +0000

    try to fix requirement check

--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -3,7 +3,7 @@ def uniq_add(my_list=[]):
     total = 0
     unique_list = list(set(my_list))
     if not my_list:
-        return (0)
+        return (None)
     for i in range(len(unique_list)):
         total += unique_list[i]
     return (total)
