101959d81a7384ac2de7464005369d91f97f2459
shannonanderson77 <shannonanderson77@gmail.com>
2018-04-23 03:55:15 +0000

    change to pythonic way

--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -1,9 +1,6 @@
 #!/usr/bin/python
 def uniq_add(my_list=[]):
     total = 0
-    unique_list = list(set(my_list))
-    if not my_list:
-        return (None)
-    for i in range(len(unique_list)):
-        total += unique_list[i]
+    for item in set(my_list):
+        total += item
     return (total)
