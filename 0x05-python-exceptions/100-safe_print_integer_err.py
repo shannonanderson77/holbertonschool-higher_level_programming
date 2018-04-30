502491484da557537abba6d76c5c4d403557d9d8
shannonanderson77 <shannonanderson77@gmail.com>
2018-04-30 19:25:39 +0000

    fix syntax

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
 import sys
 
+
 def safe_print_integer_err(value):
     try:
         print("{:d}".format(value))
         return (True)
-    except ValueError as error:
+    except Exception as error:
         sys.stderr.write("Exception: {}\n".format(error))
         return (False)
