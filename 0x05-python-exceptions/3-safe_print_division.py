633c73b8d88c259771e425a560bf39b98e7a84fd
shannonanderson77 <shannonanderson77@gmail.com>
2018-05-01 05:00:21 +0000

    fix more requirements, print statement only in finally block

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -4,8 +4,7 @@ def safe_print_division(a, b):
     try:
         result = a / b
     except ZeroDivisionError:
-        print("Inside result: None")
         return (None)
     finally:
-        print("Inside result: {:.1f}".format(result))
+        print("Inside result: {}".format(result))
         return (result)
