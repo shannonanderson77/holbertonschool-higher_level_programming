f1484e83479c3868d03d8c80a1e829e2dfdd5a20
shannonanderson77 <shannonanderson77@gmail.com>
2018-05-01 05:24:16 +0000

    add new line to exception print statement

--- 0x05-python-exceptions/101-safe_function.py
+++ 0x05-python-exceptions/101-safe_function.py
@@ -5,7 +5,7 @@ import sys
 def safe_function(fct, *args):
     try:
         result = fct(*args)
-        return (result)
     except Exception as error:
-        sys.stderr.write("Exception: {}".format(error))
+        sys.stderr.write("Exception: {}\n".format(error))
         return (None)
+    return (result)
