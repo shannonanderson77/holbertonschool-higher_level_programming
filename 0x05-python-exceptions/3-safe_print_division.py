d631d681e5fae34473f2fa7ee515f2ad7ddaf2a0
shannonanderson77 <shannonanderson77@gmail.com>
2018-05-01 03:54:54 +0000

    move print statement to finally block per requirements, remove extra space in exception print statement

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -3,9 +3,9 @@ def safe_print_division(a, b):
     result = None
     try:
         result = a / b
-        print("Inside result: {:.1f}".format(result))
     except ZeroDivisionError:
-        print("Inside result:  None")
+        print("Inside result: None")
         return (None)
     finally:
+        print("Inside result: {:.1f}".format(result))
         return (result)
