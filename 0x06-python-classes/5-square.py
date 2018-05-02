6f9007c50c0884cb15bcb73ad1358c170710cdde
shannonanderson77 <shannonanderson77@gmail.com>
2018-05-02 01:09:27 +0000

    add printing of square to stdout

---
+++ 0x06-python-classes/5-square.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3
+'''
+An empty class Square
+'''
+
+
+class Square():
+    def __init__(self, size=0):
+        '''Instantiation of private instance attribute size
+        '''
+        self.__size = size
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        self.__size = value
+        if type(self.__size) is not int:
+            raise TypeError('size must be an integer')
+        elif self.__size < 0:
+            raise ValueError('size must be >= 0')
+
+    def area(self):
+        '''A method that returns the area of a square
+        '''
+        return (self.__size * self.__size)
+
+    def my_print(self):
+        '''A method that prints a square
+        '''
+        if self.__size == 0:
+            print ("\n")
+        else:
+            for i in range(self.__size):
+                print("#" * self.__size)
