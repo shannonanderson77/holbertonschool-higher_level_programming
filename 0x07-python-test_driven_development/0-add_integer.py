ed5134222cbce9ed713fa51684ad083fd5f05e31
shannonanderson77 <shannonanderson77@gmail.com>
2018-05-06 06:26:12 +0000

    add docstrings

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,5 +1,13 @@
 #!/usr/bin/python3
+'''
+
+A method that adds two integers
+
+'''
+
+
 def add_integer(a, b=98):
+    '''function that adds two integers with exception handling'''
     if type(a) is float:
         a = int(a)
     if type(b) is float:
