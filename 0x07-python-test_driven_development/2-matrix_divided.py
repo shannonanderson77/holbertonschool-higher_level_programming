5360afebd1d92e4d00931207ed92cd43957bfe77
shannonanderson77 <shannonanderson77@gmail.com>
2018-05-07 04:06:44 +0000

    add tests for missing one argument, missing 2 arguments, and NaN

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -8,18 +8,20 @@ A method that adds two integers
 
 def matrix_divided(matrix, div):
     '''function that adds two integers with exception handling'''
+    if matrix is None:
+        raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
+    if matrix == []:
+        raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
+    if type(matrix) is not list:
+        raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
     for row in matrix:
         for column in row:
-            if div == 0:
-                raise ZeroDivisionError("division by zero")
-            if matrix == None:
-                raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
-            if type(matrix) is not list:
-                raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
-            if matrix[0] != matrix[1]:
+            if len(matrix[0]) != len(matrix[1]):
                 raise TypeError("Each row of the matrix must have the same size")
-            if type(div) is not int or float:
-                raise TypeError("div must be a number")
             if type(row) is not list:
                 raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
+    if div == 0:
+        raise ZeroDivisionError("division by zero")
+    if type(div) is not int or float:
+        raise TypeError("div must be a number")
     return [[round(column / div, 2) for column in row] for row in matrix]
