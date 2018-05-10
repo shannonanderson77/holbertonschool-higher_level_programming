a86c7df50efd115d12dbe4de04aae33308b7e52f
shannonanderson77 <shannonanderson77@gmail.com>
2018-05-10 00:04:01 +0000

    fix typo in error message

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -20,7 +20,7 @@ class Rectangle:
         if type(value) is not int:
             raise TypeError("width must be an ineger")
         elif value < 0:
-            raise TypeError("width must be >= 0")
+            raise ValueError("width must be >= 0")
         else:
             self.__width = value
 
