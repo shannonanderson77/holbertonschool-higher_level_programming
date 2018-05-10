bdaaf303e272e46c2402265f0e6cd91222d9311d
shannonanderson77 <shannonanderson77@gmail.com>
2018-05-10 00:14:33 +0000

    fix first init

--- 0x08-python-more_classes/2-rectangle.py
+++ 0x08-python-more_classes/2-rectangle.py
@@ -7,8 +7,8 @@
 class Rectangle:
     '''A Rectangle class'''
     def __init__(self, width=0, height=0):
-        self.__width = width
-        self.__height = height
+        self.width = width
+        self.height = height
 
     @property
     def width(self):
@@ -17,11 +17,12 @@ class Rectangle:
 
     @width.setter
     def width(self, value):
-        self.__width = value
-        if type(self.__width) is not int:
+        if type(value) is not int:
             raise TypeError("width must be an integer")
-        if self.__width < 0:
+        elif value < 0:
             raise ValueError("width must be >= 0")
+        else:
+            self.__width = value
 
     @property
     def height(self):
@@ -30,11 +31,12 @@ class Rectangle:
 
     @height.setter
     def height(self, value):
-        self.__height = value
-        if type(self.__height) is not int:
+        if type(value) is not int:
             raise TypeError("height must be an integer")
-        if self.__height < 0:
+        if value < 0:
             raise ValueError("height must be >= 0")
+        else:
+            self.__height = value
 
     def area(self):
         '''Function that returns the area of a square'''
