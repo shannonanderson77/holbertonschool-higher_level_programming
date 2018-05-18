4b8fa0df9408e9920ac2866953db5324c5ba9c85
shannonanderson77 <shannonanderson77@gmail.com>
2018-05-18 00:52:34 +0000

    implement area and string representation

---
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+'''
+   A class Rectangle that inherits from BaseGeometry
+'''
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+
+
+class Rectangle(BaseGeometry):
+    def __init__(self, width, height):
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
+        self.__width = width
+        self.__height = height
+
+    def area(self):
+        return self.__width * self.__height
+
+    def __str__(self):
+        return ("[{:s}] {:d}/{:d}".format(
+           type(self).__name__, self.__width, self.__height))
