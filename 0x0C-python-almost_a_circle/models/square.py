e7ea14b6f0e5860371ee01e561717bc34353b2af
shannonanderson77 <shannonanderson77@gmail.com>
2018-05-23 02:11:21 +0000

    fix super init function call

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -11,9 +11,7 @@ class Square(Rectangle):
     ''' A Square class'''
 
     def __init__(self, size, x=0, y=0, id=None):
-        super().__init__(size, size)
-        self.x = x
-        self.y = y
+        super().__init__(size, size, x, y, id)
 
     @property
     def size(self):
@@ -25,8 +23,9 @@ class Square(Rectangle):
         self.height = value
 
     def __str__(self):
-        ''' A string representation of a Rectangle object'''
-        return "[Square] ({}) {}/{} - {}".format(self.id, self.x, self.y, self.width)
+        ''' A string representation of a Square object'''
+        return "[Square] ({}) {}/{} - {}".format(
+           self.id, self.x, self.y, self.width)
 
     def update(self, *args, **kwargs):
         ''' A function that assigns an argument to each attribute'''
